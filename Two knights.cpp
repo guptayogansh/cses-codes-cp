4fc3892f73901d659ee2a4c2d82b04f439ea0bc7
Yogansh Gupta <yogansh.11@gmail.com>
2020-07-19 00:18:20 +0530

    knights attacking maths problem

---
+++ Two knights.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+	cin >> n;
+    for (int k = 1; k <= n; k++)
+    {
+        ll a1 = k * k, a2 = a1 * (a1 - 1) / 2;
+        if (k > 2)
+            a2 -= 4 * (k - 1) * (k - 2);
+        cout << a2 << "\n";
+    }
+}
