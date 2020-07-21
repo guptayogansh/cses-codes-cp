48cb71a106b6b528082f651da6465cf11d7506be
Yogansh Gupta <yogansh.11@gmail.com>
2020-07-21 11:40:51 +0530

    final update to Two Sets

--- Two Sets.cpp
+++ Two Sets.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
-#define ll long long
 using namespace std;
-
+ 
 int main() { 
-  
-	ll n;
+ 
+	int n;
 	cin>>n;
-     if(n*(n+1)/2%2){  
-		 cout<<"NO";
-		 return 0;
-	}
+	if((n*(n+1)/2)%2){ 
+     cout<< "NO";
+	 return 0;
+   }
+ 
 	vector<int>v1,v2;
 	int j=0;
 	if(n%4)
-		j=3;
+		j=3, v1.push_back(1),v1.push_back(2),v2.push_back(3);
 	else
-		j=4;
+		j=4,v1.push_back(1),v1.push_back(4),v2.push_back(2),v2.push_back(3);
 	for(int i=0;i<(n-1)/4;i++){ 
       
 		 v1.push_back(4*i+1+j);
@@ -23,27 +23,15 @@ int main() {
 		 v2.push_back(4*i+2+j);
 		 v2.push_back(4*i+3+j);
 	}
-
-	if(n%4){ 
-      v1.push_back(1);
-      v1.push_back(2);
-      v2.push_back(3);
-	}  
-    else{
-     
-      v1.push_back(1);
-      v1.push_back(4);
-      v2.push_back(3);
-      v2.push_back(2);
-	} 
+ 
 	cout<<"YES\n";
 	cout<<v1.size() <<"\n";
-	for(auto i:v1)
+   	for(auto i:v1)
 		cout<<i<<" ";
 	cout<<"\n";
-
+	
 	cout<<v2.size() <<"\n";
-	for(auto i:v2)
+   	for(auto i:v2)
 		cout<<i<<" ";
 	cout<<"\n";
 }
