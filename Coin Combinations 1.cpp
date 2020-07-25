3f07953f72eb7255a8e5f3cae04f3021440d1754
Yogansh Gupta <yogansh.11@gmail.com>
2020-07-26 00:00:48 +0530

    ways to make a minimum sum

---
+++ Coin Combinations 1.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+
+const int mxN=100, mxX=1e6,M=1e9+7;
+int c[mxN],n,x;
+
+ll dp[mxX+1];
+
+int main() { 
+    cin>>n>>x;
+    for(int i=0;i<n;i++)
+	 cin>>c[i];	
+	dp[0]=1;
+	for(int i=1;i<=x;i++){
+		for(int j=0;j<n;j++)
+			if(c[j]<=i)
+				dp[i]=(dp[i]+dp[i-c[j]])%M;
+	}
+	cout<<dp[x];
+}
